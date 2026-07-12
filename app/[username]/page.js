34f569aa3812d5881e73367c432d4d022c61066d
Luis Guilherme Borges <luisguilhermeborges@example.com>
2026-07-12 03:12:09 -0300

    feat: complete brand SVG icons for socials row

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -275,23 +275,43 @@ export default function UserProfile({ params: paramsPromise, searchParams: searc
           {/* Social Links Icons Row */}
           <div className="profile-social-icons-centered" style={{ display: 'flex', gap: '14px', marginBottom: '28px', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
             {user.socials?.instagram && (
-              <a href={`https://instagram.com/${user.socials.instagram}`} target="_blank" className="social-circle-btn">
-                📸
+              <a href={`https://instagram.com/${user.socials.instagram}`} target="_blank" className="social-circle-btn" title="Instagram">
+                <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.051.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/></svg>
+              </a>
+            )}
+            {user.socials?.x && (
+              <a href={`https://x.com/${user.socials.x}`} target="_blank" className="social-circle-btn" title="X (Twitter)">
+                <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
               </a>
             )}
             {user.socials?.twitch && (
-              <a href={`https://twitch.tv/${user.socials.twitch}`} target="_blank" className="social-circle-btn">
-                🎮
+              <a href={`https://twitch.tv/${user.socials.twitch}`} target="_blank" className="social-circle-btn" title="Twitch">
+                <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z"/></svg>
               </a>
             )}
             {user.socials?.youtube && (
-              <a href={`https://youtube.com/${user.socials.youtube}`} target="_blank" className="social-circle-btn">
-                🎥
+              <a href={`https://youtube.com/${user.socials.youtube}`} target="_blank" className="social-circle-btn" title="YouTube">
+                <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M23.498 6.163a3.003 3.003 0 00-2.11-2.11C19.528 3.545 12 3.545 12 3.545s-7.528 0-9.388.508a3.003 3.003 0 00-2.11 2.11C0 8.022 0 12 0 12s0 3.978.502 5.837a3.003 3.003 0 002.11 2.11c1.86.508 9.388.508 9.388.508s7.528 0 9.388-.508a3.003 3.003 0 002.11-2.11C24 15.978 24 12 24 12s0-3.978-.502-5.837zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>
+              </a>
+            )}
+            {user.socials?.steam && (
+              <a href={`https://steamcommunity.com/id/${user.socials.steam}`} target="_blank" className="social-circle-btn" title="Steam">
+                <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M11.979 0A12 12 0 000 11.995c0 5.485 3.68 10.114 8.74 11.533l1.104-4.575a3.46 3.46 0 01-.194-1.129c0-1.802 1.402-3.284 3.178-3.435l2.457-3.522a3.46 3.46 0 01.077-4.88 3.42 3.42 0 014.826 0 3.46 3.46 0 010 4.88 3.42 3.42 0 01-4.801.054l-3.513 2.454a3.45 3.45 0 01-1.144.179 3.46 3.46 0 01-3.232-2.222l-4.492-1.3A11.97 11.97 0 0012 23.99c6.627 0 12-5.373 12-12S18.607 0 11.979 0zM7.35 15.006l4.24 1.23a1.92 1.92 0 001.81-1.222 1.94 1.94 0 00-1.81-2.58h-.024a1.92 1.92 0 00-1.786 1.226l-4.225-1.224a9.7 9.7 0 011.795-3.77z"/></svg>
+              </a>
+            )}
+            {user.socials?.spotify && (
+              <a href={`https://open.spotify.com/user/${user.socials.spotify}`} target="_blank" className="social-circle-btn" title="Spotify">
+                <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M12 0C5.373 0 0 5.372 0 12s5.373 12 12 12 12-5.372 12-12S18.627 0 12 0zm5.489 17.309c-.218.359-.687.472-1.046.25-2.903-1.776-6.557-2.177-10.858-1.194-.41.093-.82-.166-.913-.576-.093-.41.167-.82.576-.913 4.708-1.076 8.736-.615 11.99 1.38.36.218.472.686.251 1.043zm1.467-3.262c-.275.446-.86.591-1.306.315-3.323-2.043-8.39-2.634-12.31-1.444-.501.15-1.029-.133-1.18-.634-.15-.501.134-1.029.634-1.18 4.49-1.36 10.07-.7 13.85 1.624.446.275.592.86.312 1.319zm.126-3.374C14.73 7.822 7.55 7.585 3.393 8.847c-.64.195-1.316-.164-1.511-.806-.195-.64.164-1.316.806-1.51 4.78-1.452 12.72-1.173 17.74 1.808.577.342.766 1.09.424 1.666-.342.578-1.094.767-1.664.425z"/></svg>
+              </a>
+            )}
+            {user.socials?.kick && (
+              <a href={`https://kick.com/${user.socials.kick}`} target="_blank" className="social-circle-btn" title="Kick">
+                <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M0 0h5.085v3.39H8.47v3.39h3.39V0h5.085v6.78h-3.39v3.39h3.39v6.78H13.56v3.39H8.47v-3.39H5.085V24H0V0zm18.915 6.78H24v10.44h-5.085V6.78z"/></svg>
               </a>
             )}
             {user.socials?.discord && (
-              <a href={`https://discord.com/users/${user.id}`} target="_blank" className="social-circle-btn">
-                💬
+              <a href={`https://discord.com/users/${user.id}`} target="_blank" className="social-circle-btn" title="Discord">
+                <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M20.317 4.37a19.791 19.791 0 00-4.885-1.515.074.074 0 00-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 00-5.487 0 12.64 12.64 0 00-.617-1.25.077.077 0 00-.079-.037A19.736 19.736 0 003.677 4.37a.07.07 0 00-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 00.031.057 19.9 19.9 0 005.993 3.03.078.078 0 00.084-.028 14.09 14.09 0 001.226-1.994.076.076 0 00-.041-.106 13.107 13.107 0 11-1.873-.894.077.077 0 01-.008-.128c.126-.093.252-.19.372-.287a.075.075 0 01.077-.011c3.92 1.793 8.18 1.793 12.061 0a.073.073 0 01.078.009c.12.099.246.195.373.289a.077.077 0 01-.006.127 12.299 12.299 0 01-1.873.894.077.077 0 00-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 00.084.028 19.839 19.839 0 006.002-3.03.077.077 0 00.032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 00-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.156-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.156 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.156-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.156 2.418z"/></svg>
               </a>
             )}
             {currentUser && currentUser.id === user.id ? (
@@ -299,7 +319,7 @@ export default function UserProfile({ params: paramsPromise, searchParams: searc
                 ➕
               </Link>
             ) : (
-              <span className="social-circle-btn-more">+5</span>
+              <span className="social-circle-btn-more">-5</span>
             )}
           </div>
 
